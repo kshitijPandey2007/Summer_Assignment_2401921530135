fde10ce54c2ee2151fe0814569cca335fd17331c
kshitijPandey2007 <pandeykshitij55@gmail.com>
2026-06-13 20:36:11 +0530

    valid anagram added

---
+++ week2/day1/question1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size()!=t.size()) return false;
+        vector<int> ans(26,0);
+        for (int i=0;i<s.size();i++){
+            ans[s[i]-'a']++;
+            ans[t[i]-'a']--;
+        }
+        for (int i=0;i<26;i++){
+            if (ans[i]) return false;
+        }
+        return true;
+        
+    }
+};
